cfe56c896c1aef5df5d058e655c30379c33a7c27
djysyed <whysigh729@gmail.com>
2020-12-21 16:02:25 -0800

    removed unnecessary files

--- first_game/first_game/command.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  command.cpp
-//  first_game
-//
-//  Created by Yunus Syed on 12/6/20.
-//  Copyright © 2020 Yunus Syed. All rights reserved.
-//
-
-#include "command.hpp"
--- first_game/first_game/resource_holder.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  resource_holder.cpp
-//  first_game
-//
-//  Created by Yunus Syed on 11/13/20.
-//  Copyright © 2020 Yunus Syed. All rights reserved.
-//
-
-#include "resource_holder.hpp"
